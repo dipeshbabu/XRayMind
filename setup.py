3d7e2d58e8aae8055709602573f97f748449f55b
dipeshbabu <dipeshmahato@outlook.com>
2023-05-14 21:17:58 +0545

    add setups

---
+++ setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="xray-tools",
+    author="",
+    version="0.1",
+    packages=find_packages(),
+    install_requires=[
+        "torch>=1.12.1",
+        "numpy>=1.21.6",
+        "torchvision>=0.13.1",
+        "matplotlib>=3.2.2",
+        "torchxrayvision>=0.0.38",
+        "captum>=0.5.0",
+        "gradio>=3.4.1",
+        "sklearn",
+        "pandas",
+    ],
+)
