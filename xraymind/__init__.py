11d808fcd8df7920440da8326acd08f6387f5600
Dipesh Babu <59379458+dipeshbabu@users.noreply.github.com>
2026-04-27 03:34:28 -0400

    Bump package version to v0.9.0

--- xraymind/__init__.py
+++ xraymind/__init__.py
@@ -1,5 +1,5 @@
 """XRayMind: explainable chest X-ray inference, reporting, reliability, and uncertainty utilities."""
 
-__version__ = "0.5.0"
+__version__ = "0.9.0"
 
 DEFAULT_MODEL = "densenet121-res224-all"
